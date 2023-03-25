f0e89ea3e9b5b04746adaaa79f841a92c742be6f
Mihail Feraru <mihailferaru2000@gmail.com>
2023-03-25 21:48:35 +0200

    Add regex expressions for text cleanup

--- src/preprocess.py
+++ src/preprocess.py
@@ -9,6 +9,13 @@ from typing import List, Set, Dict, TypeVar, Generic
 
 PT = TypeVar('PT')
 
+url_regex = "https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+twitter_handle_regex = '@[a-zA-Z0-9_-]+'
+
+
+def remove_by_regex(df: DataFrame, column: str, regex: str):
+    df[column] = df[column].str.replace(regex, '', regex=True)
+
 
 class TextPreprocessor(ABC, Generic[PT]):
     def __init__(self, dataset: DataFrame) -> None:
@@ -25,7 +32,7 @@ class BertPreprocessor(TextPreprocessor[BatchEncoding]):
 
         # 94 chrs, media 120chr - 20 mean toks
         self.tokenizer = AutoTokenizer.from_pretrained(repo)
-        self.max_length = 32 # TODO: adjust this?
+        self.max_length = 32  # TODO: adjust this?
 
     def __call__(self, dataset: DataFrame) -> BatchEncoding:
         # Manual preprocessing
@@ -49,13 +56,14 @@ class BertPreprocessor(TextPreprocessor[BatchEncoding]):
             return_tensors='pt'
         )
 
+
 class RobertaPreprocessor(TextPreprocessor[BatchEncoding]):
     def __init__(self, dataset: DataFrame, repo: str) -> None:
         super().__init__(dataset=dataset)
 
         # 94 chrs, media 120chr - 20 mean toks
         self.tokenizer = AutoTokenizer.from_pretrained(repo, use_fast=True)
-        self.max_length = 256 # TODO: adjust this?
+        self.max_length = 256  # TODO: adjust this?
 
     def __call__(self, dataset: DataFrame) -> BatchEncoding:
         # Manual preprocessing
