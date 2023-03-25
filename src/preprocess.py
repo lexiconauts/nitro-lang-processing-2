c897785099c9365ef9dc259d6707fad96e0a3035
Handrosoft <hello@handrosoft.com>
2023-03-25 18:35:32 +0200

    local changes

--- src/preprocess.py
+++ src/preprocess.py
@@ -48,3 +48,33 @@ class BertPreprocessor(TextPreprocessor[BatchEncoding]):
             return_attention_mask=True,
             return_tensors='pt'
         )
+
+class RobertaPreprocessor(TextPreprocessor[BatchEncoding]):
+    def __init__(self, dataset: DataFrame, repo: str) -> None:
+        super().__init__(dataset=dataset)
+
+        # 94 chrs, media 120chr - 20 mean toks
+        self.tokenizer = AutoTokenizer.from_pretrained(repo, use_fast=True)
+        self.max_length = 256 # TODO: adjust this?
+
+    def __call__(self, dataset: DataFrame) -> BatchEncoding:
+        # Manual preprocessing
+        sentences = []
+        for text in dataset['text'].tolist():
+            text = text \
+                .replace("ţ", "ț") \
+                .replace("ş", "ș") \
+                .replace("Ţ", "Ț") \
+                .replace("Ş", "Ș")
+            sentences.append(text)
+
+        # Retrieve max-length
+        return self.tokenizer.__call__(
+            sentences,
+            add_special_tokens=True,
+            padding='max_length',
+            max_length=self.max_length,
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt'
+        )
