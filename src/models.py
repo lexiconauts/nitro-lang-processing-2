e1e2c2623b199b938fb6459ff4431cb07416000a
fusedbloxxer <voidwarden@yahoo.com>
2023-03-25 16:12:30 +0200

    Fix dataset bug for slice

--- src/models.py
+++ src/models.py
@@ -2,24 +2,47 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 from transformers import AutoModel
-from typing import Dict
+from transformers import BertModel
+from typing import Dict, List, Set, Tuple
 
 
 class BertFlatClassModel(nn.Module):
-    def __init__(self, repo: str, *args, **kwargs) -> None:
+    def __init__(self,
+                 repo: str,
+                 unfreeze: str | List[str] = 'all',
+                 *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         # Init the base model
-        self.model = AutoModel.from_pretrained(repo)
-        self.modify_structure()
-        self.unfreeze()
+        self.n_classes = 5
+        self.bert_model: BertModel = AutoModel.from_pretrained(repo)
+        self.unfreeze(unfreeze)
+        self.create_layers()
 
-    def modify_structure(self) -> None:
-        pass
+    def create_layers(self) -> None:
+        self.layers = nn.Sequential(
+            nn.Linear(in_features=768, out_features=self.n_classes)
+        )
 
-    def unfreeze(self) -> None:
-        pass
+    def unfreeze(self, layers: str | List[str], unfreeze: bool = True) -> None:
+        if layers == 'all':
+            for param in self.parameters():
+                param.requires_grad = unfreeze
+        else:
+            for (param_name, param) in self.named_parameters():
+                if param_name in layers:
+                    param.requires_grad = unfreeze
 
     def forward(self, x: Dict[str, Tensor]):
-        pass
+        # Extract the relevant data
+        input_ids = x['input_ids']
+        attention_mask = x['attention_mask']
+
+        # Call the pretrained model
+        _, output = self.bert_model(input_ids, attention_mask)
+
+        # Add layers over it
+        output = self.layers(output)
+
+        return output
 
